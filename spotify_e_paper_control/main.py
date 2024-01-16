1da33c3d9ba782fee797b362f3790b0769ca52f1
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2024-01-16 19:47:25 +0000

    added ui

--- spotify_e_paper_control/main.py
+++ spotify_e_paper_control/main.py
@@ -17,18 +17,24 @@ class App:
         self.flag_t = 1
         self.i = self.j = self.k = self.ReFlag = self.SelfFlag = 0
 
-        # self.fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-        # self.font15 = ImageFont.truetype(os.path.join(self.fontdir, 'Font.ttc'), 15)
-        # self.font24 = ImageFont.truetype(os.path.join(self.fontdir, 'Font.ttc'), 24)
-
         logging.info("epd2in13_V4 Touch Demo")
+
+        logging.info("loading UI assets")
+
+        self.ui_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'ui')
+        self.play_button = Image.open(os.path.join(self.ui_dir, 'play.png'))
+        self.pause_button = Image.open(os.path.join(self.ui_dir, 'pause.png'))
+        self.previous_button = Image.open(os.path.join(self.ui_dir, 'previous.png'))    
+        self.next_button = Image.open(os.path.join(self.ui_dir, 'previous.png')).rotate(180) # reuse previous.png reversed 180 degrees as next button
+
+        logging.info("initialising")
 
         self.epd = epd2in13_V4.EPD()
         self.gt = gt1151.GT1151()
         self.GT_Dev = gt1151.GT_Development()
         self.GT_Old = gt1151.GT_Development()
 
-        logging.info("init and Clear")
+        logging.info("initial clear")
 
         self.epd.init(self.epd.FULL_UPDATE)
         self.gt.GT_Init()
@@ -47,7 +53,7 @@ class App:
                 self.GT_Dev.Touch = 0
         print("thread:exit")
 
-    def bmp_from_jpeg_url(self, url: str):
+    def get_spotify_thumbnail(self, url: str):
         """
         Convert remote JPEG to BMP.
 
@@ -65,33 +71,61 @@ class App:
             .resize((122, 122))
         )
 
+    def layer_ui(self):
+        """
+        Use PNG assets to create layered UI, then export to BMP for display.
+        """
+
+        # create new view 122x250 with white background
+        newimage = Image.new("RGBA", (122, 250), (255, 255, 255, 1))
+
+        # add thumbnail, UI, etc.
+        # x=0, y=0 == top left, when device is oriented like a rectangle with power cord at base / bottom left
+        newimage.paste(
+            newimage, (0, 64)
+        )
+        newimage.paste(
+            self.previous_button, (31, 190)
+        )
+        newimage.paste(
+            self.next_button, (31, 60)
+        )
+
+        # export to BMP = strip alpha channel and convert to greyscale
+        newimage = newimage.convert("RGB").convert("L")
+
+        # apply new view to image
+        self.image.paste(newimage, (0, 0))
+
     def main(self):
         try:
-            image = Image.new("RGB", (122, 250), (255, 255, 255))
-            newimage = self.bmp_from_jpeg_url(
+            self.image = Image.new(
+                "RGB", (122, 250), (255, 255, 255)
+            )  # initialise with blank BG, then paste each new view as needed
+            self.thumbnail = self.bmp_from_jpeg_url(
                 "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
             )
-            image.paste(
-                newimage, (0, 64)
-            )  # position in center of screen (0,0 is top left when oriented like a rectangle with power at base)
 
-            self.epd.displayPartBaseImage(self.epd.getbuffer(image))
-            DrawImage = ImageDraw.Draw(image)
+            # build first layer
+            self.layer_ui()
+
+            self.epd.displayPartBaseImage(self.epd.getbuffer(self.image))
+            DrawImage = ImageDraw.Draw(self.image)
             self.epd.init(self.epd.PART_UPDATE)
 
             while 1:
                 if self.i > 12 or self.ReFlag == 1:
                     if self.SelfFlag == 0:
-                        self.epd.displayPartial(self.epd.getbuffer(image))
+                        self.epd.displayPartial(self.epd.getbuffer(self.image))
                     else:
-                        self.epd.displayPartial_Wait(self.epd.getbuffer(image))
+                        self.epd.displayPartial_Wait(self.epd.getbuffer(self.image))
                     self.i = 0
                     self.k = 0
                     self.j += 1
                     self.ReFlag = 0
                     print("*** Draw Refresh ***\r\n")
                 elif self.k > 50000 and self.i > 0:
-                    self.epd.displayPartial(self.epd.getbuffer(image))
+                    self.epd.displayPartial(self.epd.getbuffer(self.image))
                     self.i = 0
                     self.k = 0
                     self.j += 1
@@ -100,7 +134,7 @@ class App:
                     self.SelfFlag = 0
                     self.j = 0
                     self.epd.init(self.epd.FULL_UPDATE)
-                    self.epd.displayPartBaseImage(self.epd.getbuffer(image))
+                    self.epd.displayPartBaseImage(self.epd.getbuffer(self.image))
                     self.epd.init(self.epd.PART_UPDATE)
                     print("--- Self Refresh ---\r\n")
                 else:
